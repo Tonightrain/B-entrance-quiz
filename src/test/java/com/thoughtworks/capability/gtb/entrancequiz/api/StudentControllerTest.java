00fcbc072afd9976f7bb8d951634cf7c2d88da69
MSI-PV <805560812@qq.com>
2020-09-07 16:33:12 +0800

    增å获取学院列表接口及测试â

--- src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentControllerTest.java
@@ -1,7 +1,55 @@
 package com.thoughtworks.capability.gtb.entrancequiz.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class StudentControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    StudentController studentController;
+
+    @Autowired
+    StudentService studentService;
+
+    @Test
+    void shouldGetStudentsListSuccess() throws Exception{
+        mockMvc.perform(get("/student/list"))
+                .andExpect(jsonPath("$[0].name",is("沈乐棋")))
+                .andExpect(jsonPath("$[1].name",is("徐慧慧")))
+                .andExpect(status().isOk());
+    }
 
 }
